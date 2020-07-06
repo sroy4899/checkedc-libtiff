0e5fa2329ff6a52091701b71a7a1e61f22d15cf4
Shilpa Roy <shilparoy4899@gmail.com>
2020-07-06 12:46:08 -0400

    modified compile commands to ease conversion

--- convert_all.sh
+++ convert_all.sh
@@ -1,17 +1,10 @@
 CCONV=/Users/shilpa-roy/checkedc-clang/llvm/cmake-build-debug/bin/cconv-standalone
-INCLUDES=/Users/shilpa-roy/checkedc/include
+INCLUDES=/Users/shilpa-roy/checkedc/include/
 BASEDIR=.
 
 CPATH=$INCLUDES $CCONV \
--dump-stats \
 -output-postfix=checked \
--dump-intermediate \
--extra-arg-before=-DLUA_COMPAT_5_2 \
--extra-arg-before=-DLUA_USE_LINUX \
--extra-arg-before=-w \
 -base-dir="$BASEDIR" \
-$BASEDIR/mkg3states.c \
-$BASEDIR/mkspans.c \
 $BASEDIR/tif_aux.c \
 $BASEDIR/tif_close.c \
 $BASEDIR/tif_codec.c \
